40d943575ba83bb88f5abe683eeef91f010e4e7d
BhanukaGunawardhana <bhanukashehan9@.com>
2022-09-04 00:14:13 +0530

    BB

--- src/DataStructures/Getinput.java
+++ src/DataStructures/Getinput.java
@@ -2,31 +2,25 @@ package DataStructures;
 
 public class Getinput {
     int []array={-10,25,50,60,-40,80,-22};
-    public int maxfind(int [] array){
+    public void maxfind(int [] array){
         int max=array[0];
+        int min=array[0];
 
         for(int i=0;i<array.length;i++){
             if(array[i]>=max){
                 max=array[i];
 
             }
-
-        }
-        return max;
-
-
-    }
-    public int minfind(int []array){
-        int min=array[0];
-
-        for(int i=0;i<array.length;i++){
             if(array[i]<=min){
                 min=array[i];
 
-            }
+        }
 
         }
-        return min;
+
+        System.out.println("Max :"+max);
+        System.out.println("Min :"+min);
+
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int []array={-10,25,50,60,-40,80,-22};
 
         Getinput input=new Getinput();
-        System.out.println(input.maxfind(array));
-        System.out.println(input.minfind(array));
+        input.maxfind(array);
+        //System.out.println(input.minfind(array));
     }
 }
